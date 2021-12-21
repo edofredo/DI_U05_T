0e74ea6f20b29accc41379a5d64d971a255778b8
cristian <cristianademed@hotmail.com>
2021-12-21 10:56:54 +0100

    Clase AlumnosDAM

--- src/modelo/AlumnosDAM.java
+++ src/modelo/AlumnosDAM.java
@@ -9,6 +9,52 @@ package modelo;
  *
  * @author Cristian
  */
-public class AlumnosDAM {
+public class AlumnosDAM implements Comparable<AlumnosDAM>{
+   private String dni, modulo, provincia;
+   private double nota;
+
+    public AlumnosDAM(String dni, String modulo, String provincia, double nota) {
+        this.dni = dni;
+        this.modulo = modulo;
+        this.provincia = provincia;
+        this.nota = nota;
+    }
    
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getModulo() {
+        return modulo;
+    }
+
+    public void setModulo(String modulo) {
+        this.modulo = modulo;
+    }
+
+    public String getProvincia() {
+        return provincia;
+    }
+
+    public void setProvincia(String provincia) {
+        this.provincia = provincia;
+    }
+
+    public double getNota() {
+        return nota;
+    }
+
+    public void setNota(double nota) {
+        this.nota = nota;
+    }
+
+    @Override
+    public int compareTo(AlumnosDAM a) {
+        return modulo.compareTo(a.getModulo());
+    }
+      
 }
